b4a66a34a134b9b12ba76bd05ed256c6b2941a57
Clément W <git@clmntw.fr>
2021-03-31 09:53:12 +0200

    feat(activity): prepare the mapkit view

--- Events/Utils/LocationUtils.swift
+++ Events/Utils/LocationUtils.swift
@@ -1,6 +1,10 @@
 import MapKit
 
 class LocationUtils {
+    /// Recover the GPS location of a human readable address
+    /// - Parameters:
+    ///   - address: a human readable existing address
+    ///   - completion: callback when the location is resolved
     static func coordinates(forAddress address: String, completion: @escaping (CLLocationCoordinate2D?) -> Void) {
         let geocoder = CLGeocoder()
         geocoder.geocodeAddressString(address) {
--- Events/Views/ActivitiesDetails.swift
+++ Events/Views/ActivitiesDetails.swift
@@ -1,14 +1,32 @@
 
 import SwiftUI
+import MapKit
+
+// Extends it to make it work with MapKit pin (Requires Identifiable)
+extension CLLocationCoordinate2D: Identifiable {
+    public var id: String {
+        "\(latitude)-\(longitude)"
+    }
+}
 
 struct ActivitiesDetails: View {
     var activity: Activity
+    
+    init(activity: Activity) {
+        self.activity = activity;
+    }
+    
     static let taskDateFormat: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateStyle = .medium
         formatter.timeStyle = .short
         return formatter
     }()
+    
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 0, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+    
+    @State private var annotations: Array<CLLocationCoordinate2D> = []
+
     var body: some View {
         ScrollView {
             VStack(alignment: .leading) {
@@ -31,11 +49,32 @@ struct ActivitiesDetails: View {
                 
                 Text("Starting \(activity.fields.startDate, formatter: Self.taskDateFormat)")
                 Text("Ending \(activity.fields.endDate, formatter: Self.taskDateFormat)")
+                
+                Divider()
+                
+                Map(coordinateRegion: $region, annotationItems: annotations) {
+                    MapPin(coordinate: $0)
+                }
+                    .frame(width: 400, height: 300)
             }
             .padding()
         }
         .navigationTitle(activity.fields.name)
         .navigationBarTitleDisplayMode(.inline)
+        .onAppear(perform: {
+            LocationUtils.coordinates(forAddress: "TODO: Replace me pls", completion: {
+                (location) in
+                guard let location = location else {
+                    // Handle error here.
+                    print("Error smth")
+                    return
+                }
+                
+                region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude), span: MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5))
+                annotations.removeAll()
+                annotations.append(CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude))
+            })
+        })
     }
 }
 
