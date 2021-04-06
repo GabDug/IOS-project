25251785220523ab2eadbb6875f446c4d3f5bd90
RN <rubenabet@gmail.com>
2021-04-06 18:38:19 +0100

    feat: update Speaker model

--- Events/Models/SpeakerModel.swift
+++ Events/Models/SpeakerModel.swift
@@ -24,16 +24,20 @@ struct Speaker: Hashable, Codable, Identifiable {
 struct SpeakerFields: Hashable, Codable {
     var name: String
     var role: String
+    var email: String?
+    var phone: String?
     
     var type: String
     var status: String?
     
-    var company: [String]?
+    var company: [String]
     var speakingAt: [String]?
     
     enum CodingKeys: String, CodingKey {
         case name = "Name"
         case role = "Role"
+        case email = "Email"
+        case phone = "Phone"
         case type = "Type"
         case status = "Status"
         case company = "Company"
