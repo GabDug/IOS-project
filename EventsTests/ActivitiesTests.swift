292ae5ba83cb601dddf953b0c685ff3d10224218
Thibault LEPEZ <thibault.lepez@efrei.net>
2021-04-06 18:37:50 +0200

    refactor(test): remove unused functions
    also add baseline to tests

--- EventsTests/ActivitiesTests.swift
+++ EventsTests/ActivitiesTests.swift
@@ -11,14 +11,6 @@ import XCTest
 
 class ActivitiesTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testLoadingActivitesFromJSON() throws {
         let root: Root = Events.load("activities.json")
         let activities = root.activities
