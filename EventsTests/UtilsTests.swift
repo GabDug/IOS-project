165555b96b2dc76c5c9da5e464b0cb991a7f90ea
Thibault LEPEZ <thibault.lepez@efrei.net>
2021-04-06 19:41:11 +0200

    test(utils): Add utils testing

---
+++ EventsTests/UtilsTests.swift
@@ -0,0 +1,47 @@
+//
+//  UtilsTests.swift
+//  EventsTests
+//
+//  Created by Thibault Lepez on 06/04/2021.
+//
+
+import XCTest
+import MapKit
+
+@testable import Events
+
+class UtilsTests: XCTestCase {
+
+    func testDiffColorsFormDiffIds() throws {
+        let color1 = Events.ColorUtils.newColorFromId(userId: "azerty")
+        let color2 = Events.ColorUtils.newColorFromId(userId: "qwerty")
+        
+        XCTAssertNotEqual(color1, color2)
+    }
+    
+    func testInitialsFromSimpleName() throws {
+        let initals = Events.NameUtils.initialsFromName(name: "Thibault LEPEZ")
+        
+        XCTAssertEqual(initals, "TL")
+    }
+    
+    func testInitialsFromComplexName() throws {
+        let initals = Events.NameUtils.initialsFromName(name: "Thibault of the Hill")
+        
+        XCTAssertEqual(initals, "TH")
+    }
+    
+    func testLocationFromAddress() throws {
+        let expect = expectation(description: "Address converiton")
+        var coord: CLLocationCoordinate2D?
+        
+        Events.LocationUtils.coordinates(forAddress: "64 rue Saint-Roch, Villejuif, France") { coordinate in
+            coord = coordinate ?? nil
+            expect.fulfill()
+        }
+        
+        waitForExpectations(timeout: 5, handler: nil)
+        
+        XCTAssertNotNil(coord)
+    }
+}
