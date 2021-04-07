22395df7d70e05231ef45b2535e903fc034253e1
Matthieu Gédéon <matthieu.gedeon@efrei.net>
2021-04-07 08:09:21 +0100

    test: Complex navigation

--- EventsUITests/EventsUITests.swift
+++ EventsUITests/EventsUITests.swift
@@ -60,14 +60,38 @@ class EventsUITests: XCTestCase {
         
         
     }
-
-    func testPicker_FilteredElementsHaveGoodDate() throws {
-        let app = XCUIApplication()
+    
+    // Test a complex navigation of the app
+    func testNavigation_Complex() throws {
         
-        let activitiesContainerScrollView = app.scrollViews["Activities Container"]
-        let picker = activitiesContainerScrollView.pickers["Date picker"]
-        XCTAssert(picker.exists)
+        let app = XCUIApplication()
+        let elementsQuery = app.scrollViews["Activities List"].otherElements
+        elementsQuery.buttons["Panel, How to make all your devices play nice, 10:30 AM - 11:20 AM"].tap()
+        app.navigationBars["How to make all your devices play nice"].swipeUp()
+        app.navigationBars["How to make all your devices play nice"].swipeDown()
+        app.navigationBars["How to make all your devices play nice"].buttons["Activities"].tap()
+        
+        elementsQuery.buttons["Workshop, Workshop for security professionals, 11:30 AM - 12:00 PM"].tap()
+        app.navigationBars["Workshop for security professionals"].buttons["Activities"].tap()
+        
+        elementsQuery.buttons["Panel, Which security solution is best for you?, 1:30 PM - 2:20 PM"].tap()
+        let elementsQuery2 = app.scrollViews.otherElements.scrollViews.otherElements
+        elementsQuery2.buttons["SO, Stephan Oswald"].tap()
+        app.navigationBars["Stephan Oswald"].buttons["Which security solution is best for you?"].tap()
+        app.navigationBars["Which security solution is best for you?"].buttons["Activities"].tap()
+        
+        let tabBar = app.tabBars["Tab Bar"]
+        tabBar.buttons["Sponsors"].tap()
+        app.scrollViews["Sponsors Container"].otherElements.scrollViews.otherElements.buttons["NANA, National Association of Neighborhood Associations "].tap()
+        elementsQuery2.buttons["CR, Clara Rotelli"].tap()
+        app.navigationBars["Clara Rotelli"].buttons["National Association of Neighborhood Associations"].tap()
+        app.navigationBars["National Association of Neighborhood Associations"].buttons["Sponsors"].tap()
+        tabBar.buttons["Attendees"].tap()
+        app.scrollViews["Attendees Container"].otherElements.buttons["SE, Sam Epps"].tap()
+        app.navigationBars["Sam Epps"].buttons["Attendees"].tap()
+        
     }
+
 
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
